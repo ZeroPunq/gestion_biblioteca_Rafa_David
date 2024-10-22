dc36611f7d8ceb55927e35e197b27e3f17f820f0
Rafael Haro <rafaharotello2003@gmail.com>
2024-10-22 09:53:38 +0200

    Modificamos clase AutorDAO

--- DAO/src/AutorDAO.java
+++ DAO/src/AutorDAO.java
@@ -16,7 +16,7 @@ public class AutorDAO {
     }
     public ArrayList<Autor> selectAll(){
         ArrayList<Autor> autores = null;
-        String SQL = "SELECT * FROM Libro";
+        String SQL = "SELECT * FROM autor";
         try{
             PreparedStatement ps = Conexion.conectar().prepareStatement(SQL);
             ps.execute();
